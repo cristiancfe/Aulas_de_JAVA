a6b5580a26a461c4191b209c89b9879155b25838
Cristian Fabiano  Eisenhut <cristiancfe@gmail.com>
2021-08-04 17:45:59 -0300

    Aula 1 de JAVA iterasys

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,18 @@
+// 1 - Pacote
+
+package intro;
+//2 - Referencia as bibliotecas
+
+// 3 - Classe
+
+public class Medidas {
+    // 3.1 - Atributos - Características
+
+    // 3.2 - Métodos ( void - não tem retorno) e Funções ( sem void - tem retorno)
+
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro código em JAVA");
+        System.out.println("Intervalo : voltaremos !!!");
+    }
+
+}
